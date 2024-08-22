9ad8a5d9f60aca7eaf74304c9d423903d1ebdcfe
Crystal <99391710+x200706@users.noreply.github.com>
2024-08-22 22:22:53 +0800

    部署失敗修復

--- docs/.vuepress/components/CustomFooter.vue
+++ docs/.vuepress/components/CustomFooter.vue
@@ -5,7 +5,6 @@
       By Crystal {{ year }}
     </div>
   </div>
-<script type="application/javascript" defer src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
 <div class="elfsight-app-9ded2d07-fa35-442c-87e5-01e3da5a5c57" data-elfsight-app-lazy></div>
 </template>
 <script>
@@ -16,6 +15,13 @@ export default {
     return {
       year: currentYear
     }
-  }
+  },
+  mounted() {
+    let recaptchaScript = document.createElement('script');
+    recaptchaScript.setAttribute('src', 'https://static.elfsight.com/platform/platform.js');
+    recaptchaScript.setAttribute('data-use-service-core', 'true');
+    recaptchaScript.setAttribute('defer', 'true');
+    document.head.appendChild(recaptchaScript);
+  },
 }
 </script>

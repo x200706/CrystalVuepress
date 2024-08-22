7a18b6dc11896b41e40917490e52136db11b9d74
Crystal <99391710+x200706@users.noreply.github.com>
2024-08-22 22:34:04 +0800

    改版面，修復心情日記404

--- docs/.vuepress/components/CustomFooter.vue
+++ docs/.vuepress/components/CustomFooter.vue
@@ -5,7 +5,6 @@
       By Crystal {{ year }}
     </div>
   </div>
-<div class="elfsight-app-9ded2d07-fa35-442c-87e5-01e3da5a5c57" data-elfsight-app-lazy></div>
 </template>
 <script>
 const d = new Date()
@@ -15,13 +14,6 @@ export default {
     return {
       year: currentYear
     }
-  },
-  mounted() {
-    let recaptchaScript = document.createElement('script');
-    recaptchaScript.setAttribute('src', 'https://static.elfsight.com/platform/platform.js');
-    recaptchaScript.setAttribute('data-use-service-core', 'true');
-    recaptchaScript.setAttribute('defer', 'true');
-    document.head.appendChild(recaptchaScript);
-  },
+  }
 }
 </script>
--- docs/.vuepress/layouts/Layout.vue
+++ docs/.vuepress/layouts/Layout.vue
@@ -1,5 +1,14 @@
 <script setup>
 import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const recaptchaScript = document.createElement('script');
+  recaptchaScript.setAttribute('src', 'https://static.elfsight.com/platform/platform.js');
+  recaptchaScript.setAttribute('data-use-service-core', 'true');
+  recaptchaScript.setAttribute('defer', 'true');
+  document.head.appendChild(recaptchaScript);
+});
 </script>
 
 <template>
@@ -7,5 +16,6 @@ import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
     <template #page-bottom>
       <CustomFooter />
     </template>
+    <div class="elfsight-app-9ded2d07-fa35-442c-87e5-01e3da5a5c57" data-elfsight-app-lazy></div>
   </ParentLayout>
 </template>
